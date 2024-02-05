e0810da0c11ca15ddcd19efcbde17311cfc23f8f
Jay Simmons <jaysimmons280@gmail.com>
2024-02-05 17:02:05 -0600

    starting class systems

---
+++ Scripts/Entity.gd
@@ -0,0 +1,8 @@
+extends CharacterBody3D
+class_name Entity
+
+@export var HP : int = 100 #Entity Health
+@export var SP : int = 100 #Entity Skill Points
+@export var AP : int = 100 #Entity Armor
+@export var walk_speed : int = 100 #Entity Walk Speed
+@export var run_speed : int = 100 #Entity Run Speed
