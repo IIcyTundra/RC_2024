5973723605a925965e7e9f9d2aa9e563dc8fcea7
Dylan Cordes <dylancordes10@gmail.com>
2024-03-14 02:56:29 -0500

    2nd big change
    fixed some crashing,
    set up Hp

--- RC_2024-Dylan-Cordes/Scripts/PlayerCharacterBase.gd
+++ RC_2024-Dylan-Cordes/Scripts/PlayerCharacterBase.gd
@@ -11,8 +11,20 @@ var gravityNorm = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var dashNum = 2
 var medRoll = 15
-var speed = 5
 
+const initialAttackBuffer = 300
+const lightAttackTime = 1000
+var lftClickTime = 0
+var rgtClickTime = 0
+enum PlayerState{
+	Idle,
+	Light,
+	Heavy,
+	Zone
+}
+var lightCounter = 1;
+var Buffer = false;
+var currentAttack = PlayerState.Idle
 
 func gravity(delta):
 	if not is_on_floor():
@@ -30,7 +42,32 @@ func _ready():
 			#i += 1
 	#else:
 		#push_error("Ability slots are either NULL, or exceeds the current maximum")
-func _process(delta):
+func _process(delta: float) -> void:
+	var current_time = Time.get_unix_time_from_system() *1000
+	
+	
+	if Input.is_action_just_pressed("Light"):
+		if currentAttack == PlayerState.Light:
+			lightCounter += 1
+			Buffer = true
+			if(lightCounter > 4):
+				lightCounter = 1
+			print("chain attack - ", lightCounter)
+		elif currentAttack == PlayerState.Idle:
+			currentAttack = PlayerState.Light
+			lftClickTime = current_time
+			lightCounter = 1
+			print("light 1")
+		elif currentAttack != PlayerState.Idle:
+			pass
+			
+	if Input.is_action_just_pressed("Heavy"):
+		if currentAttack == PlayerState.Idle:
+			pass
+		
+	
+	if(current_HP <= 0):
+		player_death()
 	if Input.is_action_just_pressed("ability_1"):
 		if sloted_abilities[0] != null:
 			sloted_abilities[0].execute()
@@ -53,7 +90,7 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	input_dir = input_dir.normalized() # Normalize the input direction vector
 	if input_dir.length() > 0:
-		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * speed
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * current_speed
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 		#Dodge(big burst of velocity) towards direction player is moving
@@ -71,18 +108,27 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
+
 func look_at_cursor():
 	#horizontal plane, no looking up or down
 	var player_pos = global_transform.origin
 	var dropPlane = Plane(Vector3(0,1,0), player_pos.y)
+	
 	#ray blast from camera, player will look at it
-	var ray_length = 1000
+	var ray_length = 500
 	var mousePos = get_viewport().get_mouse_position()
 	var rayOrig = camera.project_ray_origin(mousePos)
 	var rayEnd = rayOrig + camera.project_ray_normal(mousePos) * ray_length
 	var cursor_Pos = dropPlane.intersects_ray(rayOrig, rayEnd)
 	#TODO: The cursor is off from the mouse, but the player looks at the mouse consistently rather than the cursor
 	#The cursor is mainly for visual feedback for the ray currently, will remove later
-	cursor.global_transform.origin = cursor_Pos + Vector3(0,1,0)
-	#the player mesh looks at it
-	$MeshInstance3D2.look_at(cursor_Pos, Vector3.UP)
+	if(cursor_Pos != null):
+		cursor.global_transform.origin = cursor_Pos + Vector3(0,1,0)
+		#the player mesh looks at it
+		$MeshInstance3D2.look_at(cursor_Pos, Vector3.UP)
+
+func player_death():
+	print("dead")
+
+func light_attack():
+	pass
